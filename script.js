602e3e3e2bedc6fdc907d2adf98e12ad79ae4413
gabaozdejon-maker <gabaozdejon@gmail.com>
2026-07-22 15:26:54 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,142 +1,63 @@
-function kerko(){
-
-let nisja = document.getElementById("nisja").value;
-let destinacioni = document.getElementById("destinacioni").value;
-
-let rezultatet = document.getElementById("bus");
+let udhetimiZgjedhur="";
 
 
-if(nisja=="" || destinacioni==""){
+function rezervo(udhetimi){
 
-alert("Zgjidh qytetin e nisjes dhe destinacionin");
-return;
-
-}
+udhetimiZgjedhur = udhetimi;
 
 
-
-let rruga = nisja + " → " + destinacioni;
-
+document.getElementById("bookingForm").style.display="block";
 
 
-if(
-(nisja=="Tiranë" && destinacioni=="Gjirokastër") ||
-(nisja=="Gjirokastër" && destinacioni=="Tiranë")
-){
+document.getElementById("tripName").innerHTML =
+"Udhëtimi: "+udhetimi;
 
 
-rezultatet.innerHTML = `
+window.scrollTo({
 
-<h2>🚌 Rezultati</h2>
-
-<div class="card">
-
-<h3>${rruga}</h3>
-
-<p>🕒 Oraret: 07:00 | 09:30 | 13:00 | 16:00</p>
-
-<p>💶 Çmimi: 1000 Lek</p>
-
-<p>📞 Kontakto kompaninë</p>
-
-</div>
-
-`;
-
-}
-
-
-else if(
-
-(nisja=="Tiranë" && destinacioni=="Sarandë") ||
-(nisja=="Sarandë" && destinacioni=="Tiranë")
-
-){
-
-
-rezultatet.innerHTML = `
-
-<h2>🚌 Rezultati</h2>
-
-<div class="card">
-
-<h3>${rruga}</h3>
-
-<p>🕒 Oraret: 05:30 | 09:00 | 14:00</p>
-
-<p>💶 Çmimi: 1500 Lek</p>
-
-<p>📞 Kontakto kompaninë</p>
-
-</div>
-
-`;
-
-}
-
-
-else{
-
-
-rezultatet.innerHTML = `
-
-<h2>🚌 Rezultati</h2>
-
-<div class="card">
-
-<h3>Nuk u gjet linjë ende</h3>
-
-<p>Na kontakto dhe do ta shtojmë këtë itinerar.</p>
-
-</div>
-
-`;
-
-}
-
-
-}
-
-
-
-
-function showSection(id){
-
-
-let section = document.getElementById(id);
-
-
-section.scrollIntoView({
+top:document.getElementById("bookingForm").offsetTop,
 
 behavior:"smooth"
 
 });
 
 
-}function rezervo(udhetimi){
+}
 
-let emri = prompt("Shkruaj emrin dhe mbiemrin:");
 
-let telefoni = prompt("Numri i telefonit:");
 
-let persona = prompt("Sa persona udhëtojnë?");
+
+function dergoRezervimin(){
+
+
+let emri=document.getElementById("emri").value;
+
+let telefoni=document.getElementById("telefoni").value;
+
+let email=document.getElementById("email").value;
+
+let persona=document.getElementById("persona").value;
+
 
 
 if(emri=="" || telefoni==""){
 
-alert("Plotëso të dhënat");
+alert("Plotëso emrin dhe telefonin");
+
 return;
 
 }
 
 
+
 alert(
-"Rezervimi u dërgua!\n\n"+
-"Udhëtimi: "+udhetimi+
+"✅ Rezervimi u dërgua!\n\n"+
+"Udhëtimi: "+udhetimiZgjedhur+
 "\nEmri: "+emri+
 "\nTelefon: "+telefoni+
 "\nPersona: "+persona
 );
+
 
 
 }
