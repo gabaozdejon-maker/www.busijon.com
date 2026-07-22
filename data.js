80ffd545ab5472557e70db74386c1c4347aa353e
gabaozdejon-maker <gabaozdejon@gmail.com>
2026-07-22 14:48:22 +0200

    Create data.js

---
+++ data.js
@@ -0,0 +1,29 @@
+const linjat = [
+
+{
+nga:"Tiranë",
+per:"Gjirokastër",
+ora:["07:00","09:30","13:00","16:00"],
+cmimi:"1000 Lek",
+kompania:"Bus Albania"
+},
+
+
+{
+nga:"Tiranë",
+per:"Sarandë",
+ora:["05:30","09:00","14:00"],
+cmimi:"1500 Lek",
+kompania:"Bus Albania"
+},
+
+
+{
+nga:"Tiranë",
+per:"Përmet",
+ora:["08:00","12:30","16:30"],
+cmimi:"800 Lek",
+kompania:"Bus Albania"
+}
+
+];
