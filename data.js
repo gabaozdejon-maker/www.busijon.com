8c3bebf0689cac05ac23f58f335ec7df68b35eb6
gabaozdejon-maker <gabaozdejon@gmail.com>
2026-07-22 14:56:37 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,29 +1,94 @@
-const linjat = [
+function kerko(){
 
-{
-nga:"Tiranë",
-per:"Gjirokastër",
-ora:["07:00","09:30","13:00","16:00"],
-cmimi:"1000 Lek",
-kompania:"Bus Albania"
-},
+let nisja = document.getElementById("nisja").value;
+let destinacioni = document.getElementById("destinacioni").value;
+
+let busSection = document.getElementById("bus");
 
 
-{
-nga:"Tiranë",
-per:"Sarandë",
-ora:["05:30","09:00","14:00"],
-cmimi:"1500 Lek",
-kompania:"Bus Albania"
-},
+if(nisja=="" || destinacioni==""){
 
+alert("Zgjidh qytetin e nisjes dhe destinacionin");
+return;
 
-{
-nga:"Tiranë",
-per:"Përmet",
-ora:["08:00","12:30","16:30"],
-cmimi:"800 Lek",
-kompania:"Bus Albania"
 }
 
-];
+
+let gjetur = linjat.filter(function(linja){
+
+return linja.nga == nisja && linja.per == destinacioni;
+
+});
+
+
+
+if(gjetur.length > 0){
+
+
+let html = "<h2>🚌 Rezultati</h2>";
+
+
+gjetur.forEach(function(linja){
+
+
+html += `
+
+<div class="card">
+
+<h3>${linja.nga} → ${linja.per}</h3>
+
+<p>🏢 Kompania: ${linja.kompania}</p>
+
+<p>🕒 Oraret: ${linja.ora.join(" | ")}</p>
+
+<p>💶 Çmimi: ${linja.cmimi}</p>
+
+<p>📞 Kontakto kompaninë</p>
+
+</div>
+
+`;
+
+
+});
+
+
+busSection.innerHTML = html;
+
+
+}
+
+else{
+
+
+busSection.innerHTML = `
+
+<h2>🚌 Rezultati</h2>
+
+<div class="card">
+
+<h3>Nuk u gjet linjë</h3>
+
+<p>Kjo linjë do të shtohet së shpejti.</p>
+
+</div>
+
+`;
+
+}
+
+
+}
+
+
+
+
+function showSection(id){
+
+document.getElementById(id).scrollIntoView({
+
+behavior:"smooth"
+
+});
+
+}
